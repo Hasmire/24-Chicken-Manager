9ec09dec52171cafccab7d5ba82008e9ec373b49
dhea <72564321+andreadayo@users.noreply.github.com>
2022-11-28 19:53:06 +0800

    Added template pages for each interface

---
+++ resources/landing.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+  <!-- TITLE -->
+  <title>Landing Template</title>
+  <link rel="icon" href="images/24chicken-icon.png" type="image/x-icon">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/template-styles.css">
+
+  <!-- FONTS -->
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&family=Open+Sans:wght@700&display=swap"
+    rel="stylesheet">
+
+  <!-- ICONS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+<div id="page-container">
+  <div id="content-wrap">
+    
+  <!-- NAVBAR -->
+  <?php include 'header-footer/header-public.php';?>
+
+  <!-- CONTENT -->
+  <div class="header">
+    <div class="content-margin">
+      <h1>Landing Page</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  </div>
+
+  <div class="paragraph content-margin">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam porta ultricies massa, vitae consectetur est venenatis sit
+    amet. Nullam accumsan ultricies tellus quis maximus. Donec urna nunc, tincidunt vel eros sed, sagittis bibendum massa.
+    Ut elementum dictum neque a rutrum. Pellentesque auctor nisl et eleifend varius. Fusce fermentum auctor pretium. Sed
+    libero quam, malesuada eu faucibus eget, volutpat vehicula nisi.<br><br>
+    
+    In vitae commodo ligula. Nulla luctus id sapien sit amet bibendum. Aenean ultricies dignissim tempor. Maecenas hendrerit
+    sollicitudin mi sed consequat. Vivamus ut dictum urna. Quisque mauris est, elementum at vestibulum quis, facilisis vitae
+    urna. Quisque ornare id orci vitae malesuada. Integer cursus tempus sem vitae porta. Morbi in pharetra orci. In lobortis
+    convallis massa eget condimentum. Nullam suscipit diam id arcu consectetur fermentum. Cras nulla libero, sodales vel
+    felis consectetur, molestie consequat mi.<br><br>
+    
+    Aliquam nec velit eget urna iaculis blandit. Etiam tempor lorem quis nunc laoreet, sed hendrerit massa consectetur.
+    Donec accumsan mi id cursus gravida. In lacinia egestas consectetur. Vestibulum fermentum ex sed hendrerit dictum.
+    Aliquam erat volutpat. Cras ac ultricies felis. Phasellus hendrerit maximus risus, at pulvinar ex luctus in.</p>
+  </div>
+  
+  <!-- FOOTER -->
+  <?php include 'header-footer/footer-public.php';?>
+  </div>
+</body>
+
+</html>
