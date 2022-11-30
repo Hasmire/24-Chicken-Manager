70ab2106609e5cb139b4f17d16577078622c0fb1
dhea <72564321+andreadayo@users.noreply.github.com>
2022-11-30 09:45:48 +0800

    Update menu.php

--- resources/menu.php
+++ resources/menu.php
@@ -10,7 +10,7 @@
   <link rel="icon" href="images/24chicken-icon.png" type="image/x-icon">
 
   <!-- CSS -->
-  <link rel="stylesheet" href="css/update-styles.css">
+  <link rel="stylesheet" href="css/menu-styles.css">
   <style>
     <?php include 'css/header-footer-styles.css'; ?>
   </style>
@@ -38,38 +38,117 @@
     </div>
   </div>
 
-  <!-- will repeat depending on the # of items -->
-  <div class="flex-box">
-    <h3 id="flavor">Flavor</h3>
-    <p id="price">₱430.00</hp3>
+  <div class="cards">
+    <!-- BACKEND: article will repeat depending on the # of items -->
+    <!-- NOTE: if they click the card, they will be redirected 
+      to the specific flavor's item.php -->
+    <!-- NOTE2: CSS only works if form is separated per card -->
+    <form action="item.php">
+      <article class="card">
+        <div class="image">
+          <img src="images/24chicken-item-sample.jpg" alt="gallery" />
+        </div>
+        <div class="card-content">
+          <div class="flavor">Flavor</div>
+          <div class="price">₱430.00</div>
+          <input type="submit" id="select" value="Select" />
+        </div>
+      </article>
+    </form>
+
+    <form action="item.php">
+      <article class="card">
+        <div class="image">
+          <img src="images/24chicken-item-sample.jpg" alt="gallery" />
+        </div>
+        <div class="card-content">
+          <div class="flavor">Flavor</div>
+          <div class="price">₱430.00</div>
+          <input type="submit" id="select" value="Select" />
+        </div>
+      </article>
+    </form>
+
+    <form action="item.php">
+      <article class="card">
+        <div class="image">
+          <img src="images/24chicken-item-sample.jpg" alt="gallery" />
+        </div>
+        <div class="card-content">
+          <div class="flavor">Flavor</div>
+          <div class="price">₱430.00</div>
+          <input type="submit" id="select" value="Select" />
+        </div>
+      </article>
+    </form>
+
+    <form action="item.php">
+      <article class="card">
+        <div class="image">
+          <img src="images/24chicken-item-sample.jpg" alt="gallery" />
+        </div>
+        <div class="card-content">
+          <div class="flavor">Flavor</div>
+          <div class="price">₱430.00</div>
+          <input type="submit" id="select" value="Select" />
+        </div>
+      </article>
+    </form>
+
+    <form action="item.php">
+      <article class="card">
+        <div class="image">
+          <img src="images/24chicken-item-sample.jpg" alt="gallery" />
+        </div>
+        <div class="card-content">
+          <div class="flavor">Flavor</div>
+          <div class="price">₱430.00</div>
+          <input type="submit" id="select" value="Select" />
+        </div>
+      </article>
+    </form>
+
+    <form action="item.php">
+      <article class="card">
+        <div class="image">
+          <img src="images/24chicken-item-sample.jpg" alt="gallery" />
+        </div>
+        <div class="card-content">
+          <div class="flavor">Flavor</div>
+          <div class="price">₱430.00</div>
+          <input type="submit" id="select" value="Select" />
+        </div>
+      </article>
+    </form>
+
+    <form action="item.php">
+      <article class="card">
+        <div class="image">
+          <img src="images/24chicken-item-sample.jpg" alt="gallery" />
+        </div>
+        <div class="card-content">
+          <div class="flavor">Flavor</div>
+          <div class="price">₱430.00</div>
+          <input type="submit" id="select" value="Select" />
+        </div>
+      </article>
+    </form>
+
+    <form action="item.php">
+      <article class="card">
+        <div class="image">
+          <img src="images/24chicken-item-sample.jpg" alt="gallery" />
+        </div>
+        <div class="card-content">
+          <div class="flavor">Flavor</div>
+          <div class="price">₱430.00</div>
+          <input type="submit" id="select" value="Select" />
+        </div>
+      </article>
+    </form>
   </div>
 
-  <div class="flex-box">
-    <h3 id="flavor">Flavor</h3>
-    <p id="price">₱430.00</hp3>
-  </div>
-
-  <div class="flex-box">
-    <h3 id="flavor">Flavor</h3>
-    <p id="price">₱430.00</hp3>
-  </div>
-
-  <div class="flex-box">
-    <h3 id="flavor">Flavor</h3>
-    <p id="price">₱430.00</hp3>
-  </div>
-
-  <div class="flex-box">
-    <h3 id="flavor">Flavor</h3>
-    <p id="price">₱430.00</hp3>
-  </div>
-
-  <div class="flex-box">
-    <h3 id="flavor">Flavor</h3>
-    <p id="price">₱430.00</hp3>
-  </div>
-
-  <a href="checkout.php"><button>Checkout</button></a>
+  <a href="checkout.php" style="text-decoration:none;"><button id="checkout">Checkout</button></a>
   
   </div>
   
