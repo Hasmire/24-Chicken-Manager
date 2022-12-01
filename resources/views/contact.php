2a2976fb7a68ad8df415c0fb9e8bf0d02d8e4abe
dhea <72564321+andreadayo@users.noreply.github.com>
2022-12-02 07:20:40 +0800

    Upload about and contact page

---
+++ resources/views/contact.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+  <!-- TITLE -->
+  <title>Contact Us</title>
+  <link rel="icon" href="../images/24chicken-icon.png" type="image/x-icon">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="../css/contact-styles.css">
+  <style>
+    <?php include '../css/header-footer-styles.css'; ?>
+  </style>
+
+  <!-- FONTS -->
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&family=Open+Sans:wght@700&display=swap"
+    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Fugaz+One&display=swap" rel="stylesheet">
+
+  <!-- ICONS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+<div id="page-container">
+  <div id="content-wrap">
+    
+  <!-- NAVBAR -->
+  <?php include '../header-footer/header-public.php';?>
+
+  <h1 id="title"><span id="yellow">Contact</span> <span id="red">Us</span></h1>
+
+  <!-- CONTENT -->
+  <div class="login content-margin">
+      <div class="login-content">
+
+        <div class="row">
+            <div class="column1">
+              <i class="fa fa-globe" aria-hidden="true"></i>
+              <h3>Official Website</h3>
+              <p>24chicken.com</p>
+            </div> 
+
+            <div class="column2">
+              <i class="fa fa-facebook-square" aria-hidden="true"></i>
+              <h3>Facebook</h3>
+              <p>facebook.com/24chickenph</p>
+            </div>
+
+            <div class="column3">
+              <i class="fa fa-instagram" aria-hidden="true"></i>
+              <h3>Instagram</h3>
+              <p>instagram.com/24chickenph</p>
+            </div> 
+        </div>  
+
+      </div>
+  </div>
+
+  
+
+  </div>
+  <!-- FOOTER -->
+  <?php include '../header-footer/footer-public.php';?>
+  </div>
+</body>
+
+</html>
