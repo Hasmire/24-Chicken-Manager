dc17890b9379d731c58a9eea169958439ef15968
Hasmirre <103040676+Hasmirre@users.noreply.github.com>
2022-12-03 13:07:25 +0800

    Revert "Admin page features"

--- resources/views/components/headerfooter/header-admin.blade.php
+++ resources/views/components/headerfooter/header-admin.blade.php
@@ -1,19 +1,13 @@
 <div class="nav">
-    <form action="/logout" method="post" >
-        @csrf
-        <button class="logout-button" type="submit" style="height:30px;font-family: 'Open Sans', sans-serif;">LOGOUT</button>
-    </form>
     <div class="content-margin">
         <input type="checkbox" id="nav-check">
 
         <div class="nav-header">
             <div class="nav-title">
-                {{-- <img src="../images/24chicken-logo.png" id="logo"> --}}
-                <img src="{{ asset('images/24chicken-logo.png') }}" id="logo">
+                <img src="../images/24chicken-logo.png" id="logo">
             </div>
-            
         </div>
-        
+
         <div class="nav-btn">
             <label for="nav-check">
                 <div class="hamburger">
@@ -23,10 +17,10 @@
                 </div>
             </label>
         </div>
-        
+
         <div class="nav-links">
-            
-            <a href="{{ route('adminpage.index') }}">Menu</a>
+            <a href=""><span id="log">Logout</span></a>
+            <a href="admin-menu.php">Menu</a>
             <a href="user-list.php">Users</a>
             <a href="admin-employee.php">Employee</a>
             <form action="/logout" method="post">
@@ -35,5 +29,4 @@
             </form>
         </div>
     </div>
-    
 </div>
