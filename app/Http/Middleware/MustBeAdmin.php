aa691665f5e6304310786569c3b73e725bc8fa48
jay <86312510+mojay2@users.noreply.github.com>
2022-12-03 16:33:46 +0800

    Create Middleware for admin and employee

---
+++ app/Http/Middleware/MustBeAdmin.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\Employee;
+
+class MustBeAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(auth()->guest()){
+            abort(Response::HTTP_FORBIDDEN);
+        }
+
+        $employees = Employee::all();
+        $user = auth()->user();
+
+        foreach($employees as $employee){
+            if($employee->user_id == $user->id){ 
+                if($employee->ismanager){
+                    return $next($request);
+                }
+                
+            }
+        }
+        abort(Response::HTTP_FORBIDDEN);
+    }
+}
